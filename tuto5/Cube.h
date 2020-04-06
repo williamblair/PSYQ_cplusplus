b538759acd200efe76b93541722d5efb547f4c6a
williamblair <bblair12876@gmail.com>
2020-04-05 20:23:48 -0400

    rewrote tut 5

---
+++ tuto5/Cube.h
@@ -0,0 +1,111 @@
+// Cube.h
+
+#ifndef CUBE_H_INCLUDED
+#define CUBE_H_INCLUDED
+
+#include <sys/types.h>
+#include <libetc.h>
+#include <libgte.h>
+#include <libgpu.h>
+#include <libpress.h>
+#include <stdlib.h>
+
+#include "System.h"
+#include "Texture.h"
+
+#define CUBESIZ 128
+
+class Cube
+{
+public:
+
+    Cube();
+
+    // Adds each of the cube faces to the system OT
+    //  (accesses the System singleton instance to add)
+    void draw();
+
+    // Rotate the amount given around each of the axes
+    void rotate(int x, int y, int z);
+
+    
+    // Load texture data into VRAM
+    void load_texture(
+        u_long *texdata,  // pointer to texture data
+        u_long *clutdata, // pointer to CLUT data (NULL if none)
+        TEXTURE_BPP bpp,  // 0 - 16bit, 1 - 8bit, 2 - 4 bit
+        int x,        // where in VRAM to load the texture
+        int y,
+        int w,        // size of the texture
+        int h,
+        int clutX,    // where to upload the clut data (if given)
+        int clutY
+    );
+
+    // Set the size of the area from the texture to use
+    // when drawing (e.g. loaded texture size is 256x256,
+    //  but you only want to use 64x64 of it)
+    void set_texture_size(u_int width, u_int height);
+
+private:
+    
+    // cube points
+    static SVECTOR P0;
+    static SVECTOR P1;
+    static SVECTOR P2;
+    static SVECTOR P3;
+    static SVECTOR P4;
+    static SVECTOR P5;
+    static SVECTOR P6;
+    static SVECTOR P7;
+
+    // cube normals vectors
+    static SVECTOR N0;
+    static SVECTOR N1;
+    static SVECTOR N2;
+    static SVECTOR N3;
+    static SVECTOR N4;
+    static SVECTOR N5;
+    static SVECTOR N6;
+    static SVECTOR N7;
+
+    // vertices - pointers to coordinates
+    static SVECTOR * vertices[6 * 4];
+
+    // normals - pointers to normals vectors
+    static SVECTOR * normals[6];
+
+
+    // Lights (local or global?)
+    static MATRIX color_mat; // light source local color matrix
+    static MATRIX light_mat; // light source local light matrix
+
+    // rotation angle for the light matrix
+    static SVECTOR light_angle;
+    // Light translation matrix
+    MATRIX light_trans_mat;
+    // The final calculated light matrix
+    MATRIX light_mat_final;
+
+    // Color of the cube (a color for each face)
+    CVECTOR colors[6];
+
+    // The primitives used for drawing
+    //  In this tutorial they are now textured (from prev tutor)
+    POLY_FT4 prims[6];
+
+    // 3D transformation objects
+    MATRIX  mat;
+    SVECTOR angle;
+    VECTOR  translation;
+
+    // Cube texture
+    Texture texture;
+
+    // The size of the portion of the texture to use when drawing
+    u_int texture_clip_width;
+    u_int texture_clip_height;
+};
+
+#endif // end ifdef CUBE_H_INCLUDED
+
--- tuto5/main.cpp
+++ tuto5/main.cpp
@@ -6,12 +6,60 @@
 #include <libgpu.h>
 #include <libpress.h>
 
-#include "tuto5.hpp"
+#include "System.h"
+#include "Sprite.h"
+#include "Cube.h"
+#include "Pad.h"
+
+// texture pattern in bgtex.c
+extern u_long bgtex[];
+
+#define ever ;;
 
 int main(void)
 {
-    tuto5 t;
-    t.run();
+    Cube              cube;
+    System *          system      = System::get_instance();
+    Pad               pad1;
 
+    // This needs to be called BEFORE system inits
+    Pad::init();
+    pad1.init_controller(0);
+
+    system->init();
+    system->init_graphics();
+    system->init_3d();
+
+    // In this tuto cube is now textured
+    cube.load_texture(bgtex+0x80,   // Texture Data
+                      bgtex,        // CLUT data
+                      TEXTURE_4BIT, // BPP
+                      640,          // VRAM tex location
+                      0,
+                      256,          // Texture size
+                      256,
+                      0,          // CLUT VRAM location
+                      480);
+
+    // The portion of the texture we want to use when drawing
+    cube.set_texture_size(64, 64);
+
+    for (ever)
+    {
+        system->start_frame();
+
+        pad1.read();
+        if (pad1.is_held(PadUp))    cube.rotate(10,0,0);
+        if (pad1.is_held(PadDown))  cube.rotate(-10,0,0);
+        if (pad1.is_held(PadLeft))  cube.rotate(0,0,10);
+        if (pad1.is_held(PadRight)) cube.rotate(0,0,-10);
+
+        cube.draw();
+
+        system->end_frame();
+    }
+
+    system->deinit();
     return 0;
 }
+
