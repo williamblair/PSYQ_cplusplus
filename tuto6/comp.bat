b2617a40cb3426aac14bc9bb47d011402ae97373
williamblair <bblair12876@gmail.com>
2020-04-08 20:43:59 -0400

    added tuto 6 (fog/depth queue lighting)

---
+++ tuto6/comp.bat
@@ -0,0 +1,14 @@
+:: batch file for compilation - psymake doesn't work on 64 bit
+
+:: compile the sources
+ccpsx -O3 -Xo$80010000 main.cpp System.cpp Sprite.cpp Sprite_textured.cpp Cube.cpp Pad.cpp Texture.cpp ../global_src/BGTEX.C -otuto6.cpe,tuto6.sym,mem.map -llibpress -llibpad
+
+:: convert into a .exe
+cpe2x tuto6.cpe
+
+:: below makes a cd, uncomment if you have psx-sdk set in your
+:: environment variables
+::copy tuto4.exe cd_root
+::systemcnf tuto4.exe > cd_root/system.cnf
+::mkisofs.exe -o tuto4.hsf -V TUTO4 -sysid PLAYSTATION cd_root
+::mkpsxiso tuto4.hsf tuto4.bin C:\psx-sdk/share/licenses/infousa.dat
